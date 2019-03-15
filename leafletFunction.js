54a759133005bfaf26c03cb5e64d18f482184c9a
KlausKeHe <uceskh0@ucl.ac.uk>
2019-03-15 08:40:20 -0400

    create thee link var

--- leafletFunction.js
+++ leafletFunction.js
@@ -1,3 +1,5 @@
 var xhrFormData;
 function startFormDataLoad() {
  xhrFormData = new XMLHttpRequest(); 
+ var url = "http://developer.cege.ucl.ac.uk:"+httpPortNumber;
+ url = url + "/getGeoJSON/formdata/geom/"+httpPortNumber;
