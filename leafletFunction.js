3168c807c8166f5e943803d50a3c69c43fd5a283
KlausKeHe <uceskh0@ucl.ac.uk>
2019-03-15 09:06:18 -0400

    add the answer id and popup

--- leafletFunction.js
+++ leafletFunction.js
@@ -52,3 +52,8 @@ feature.properties.id + ");return false;'>Submit Answer</button>";
 // now include a hidden element with the answer
  // in this case the answer is alwasy the first choice
  // for the assignment this will of course vary - you can use feature.properties.correct_answer
+ 
+ htmlString = htmlString + "<div id=answer" + feature.properties.id +
+" hidden>1</div>";
+ htmlString = htmlString + "</div>";
+ return L.marker(latlng).bindPopup(htmlString);
