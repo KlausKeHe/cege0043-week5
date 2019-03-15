55259bc563ce7e114f9897e7f2e45636ff975850
KlausKeHe <uceskh0@ucl.ac.uk>
2019-03-15 08:41:28 -0400

    holder get the request and send the requirement to server

--- leafletFunction.js
+++ leafletFunction.js
@@ -3,3 +3,6 @@ function startFormDataLoad() {
  xhrFormData = new XMLHttpRequest(); 
  var url = "http://developer.cege.ucl.ac.uk:"+httpPortNumber;
  url = url + "/getGeoJSON/formdata/geom/"+httpPortNumber;
+ xhrFormData.open("GET", url, true);
+ xhrFormData.onreadystatechange = formDataResponse;
+ xhrFormData.send();
