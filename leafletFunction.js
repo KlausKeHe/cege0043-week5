4bdcc14aac7ab468f2dc36e97357dff060824bac
KlausKeHe <uceskh0@ucl.ac.uk>
2019-03-15 08:59:22 -0400

    add the surname and module properties

--- leafletFunction.js
+++ leafletFunction.js
@@ -36,3 +36,7 @@ function loadFormData(formData) {
  // using the values in the data
  var htmlString = "<DIV id='popup'"+ feature.properties.id + "><h2>" +
 feature.properties.name + "</h2><br>";
+htmlString = htmlString + "<h3>"+feature.properties.surname +
+"</h3><br>";
+ htmlString = htmlString + "<input type='radio' name='answer' id
+='"+feature.properties.id+"_1'/>"+feature.properties.module+"<br>";
